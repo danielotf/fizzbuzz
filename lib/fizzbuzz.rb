cedc24e43b753c1d324abda53958a39cac7b84dd
EC2 Default User <ec2-user@ip-172-31-8-20.ec2.internal>
2022-05-13 14:26:07 +0000

    created test and method for multiples of three and five

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,13 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  if number % 3 == 0 && number % 5 == 0
+    return "fizzbuzz"
+  elsif number % 3 == 0
     return "fizz"
   elsif number % 5 == 0
     return "buzz"
   else return number  end
+end
+
+(1..100).each do |number|
+  puts fizzbuzz(number)
 end
