06faabc66f015b32c2892df75490d8001b6b4097
Daniel <mbatadaniel@gmail.com>
2022-05-13 15:05:20 +0100

    we wrote the test and method to return the number if it is not a multiple of 3 or 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,7 +6,7 @@ describe "fizzbuzz" do
   end
 end
 
-describe "fizzbuzz" do 
+describe "fizzbuzz" do
   it "returns 'buzz' when passed 5" do
     expect(fizzbuzz(5)).to eq "buzz"
   end
@@ -15,5 +15,13 @@ end
 describe "fizzbuzz" do
   it "returns 'fizz' when passed a multiple of 3" do
     expect(fizzbuzz(9)).to eq "fizz"
+    expect(fizzbuzz(21)).to eq "fizz"
+    expect(fizzbuzz(228)).to eq "fizz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns the argument passed when it is not a multiple of 3 or 5" do
+    expect(fizzbuzz(2)).to eq 2
   end
 end
